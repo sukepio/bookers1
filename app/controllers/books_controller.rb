1e36942b953e83a0ca6b9c8a79083d1815f9a52a
sukepio <naoto927sukena@gmail.com>
2020-12-08 12:57:35 +0000

    [Add] 投稿機能

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,24 @@
+class BooksController < ApplicationController
+  def index
+    @book = Book.new
+  end
+
+  def show
+  end
+
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to '/books/index'
+  end
+
+  def edit
+  end
+  
+  
+  private
+  
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+end
