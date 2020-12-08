326f6d1df144b26fae8137a0a4b945b1fc2a977c
sukepio <naoto927sukena@gmail.com>
2020-12-08 14:21:38 +0000

    [Ongoing] update

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -15,8 +15,14 @@ class BooksController < ApplicationController
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
   
+  def update
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    redirect_to "/books/#{book.id}"
+  end
   
   private
   
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'books' => 'books#index'
   get 'books/:id/edit' => 'books#edit'
+  post 'books/:id/update' => 'books#update'
   get 'books/:id' => 'books#show'
   
   post 'books/create'
