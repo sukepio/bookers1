7771742bdcacfe69b6864d26e9bd516b0e1e1a0c
sukepio <naoto927sukena@gmail.com>
2020-12-08 13:42:34 +0000

    [Add] 詳細画面

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'books' => 'books#index'
-  get 'books/show'
-  get 'books/edit'
+  get 'books/:id/edit' => 'books#edit'
+  get 'books/:id' => 'books#show'
   
   post 'books/create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
