4ef2104c125b775c66df5e9459b3edb1dfdf7603
sukepio <naoto927sukena@gmail.com>
2020-12-09 11:53:26 +0000

    [Start] top page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root :to => 'home#top'
   resources :books, only: [:index, :create, :edit, :show, :update, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
